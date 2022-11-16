1af0c7602d8ab87bbda5a0ee005ceec607ba38f8
Violet Yao <violetyao@CS330HW2.ntri1yosa5je5oxfryslwk3cje.xx.internal.cloudapp.net>
2022-11-16 09:25:44 +0000

    add 16 paraphrase predictions & ensemble

--- Tk-Instruct/src/ensemble_paraphrased_prompts.py
+++ Tk-Instruct/src/ensemble_paraphrased_prompts.py
@@ -1,11 +1,16 @@
+import argparse
 import json
+import random
 from collections import Counter
+import numpy as np
 from compute_metrics import compute_grouped_metrics
 
-predictions_path = "../output/textual_entailment/tk-instruct-small-def-pos/predict_eval_predictions.jsonl"
-metrics_path = "../output/textual_entailment/tk-instruct-small-def-pos/predict_results.json"
-output_path = "../output/textual_entailment/tk-instruct-small-def-pos/predict_results_best.json"
-# output_path = "../output/textual_entailment/tk-instruct-small-def-pos/predict_results_ensemble.json"
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', "--predictions_path", default="../output/textual_entailment/tk-instruct-small-def-pos/predict_eval_predictions_all_paraphrases.jsonl", help="input path to load predictions jsonl for all paraphrased prompts")
+parser.add_argument('-m', "--metrics_path", default="../output/textual_entailment/tk-instruct-small-def-pos/predict_results_all_paraphrases.json", help="input path to load metrics json for all paraphrased prompts")
+parser.add_argument('-o', "--output_path", default="../output/textual_entailment/tk-instruct-small-def-pos/predict_results_ensemble.json", help="output path to save metrics, e.g. best, ensemble, k_random_avg")
+parser.add_argument('-k', "--k", default=4, help="number of paraphrased prompts to sample")
+args = parser.parse_args()
 
 def find_majority_vote_metrics(predictions_path, output_path):
     with open(predictions_path, 'r') as json_file:
@@ -47,34 +52,63 @@ def find_majority_vote_metrics(predictions_path, output_path):
         json.dump(metrics, output_file, indent=4)
 
 def find_best_paraphrase_metrics(metrics_path, output_path):
-        metrics = json.load(open(metrics_path))
-        # group by tasks
-        metrics_by_task = {}
-        for key, value in metrics.items():
-            if not key.startswith("predict_exact_match_for_task"):
-                continue
-            task = key.split("_gpt")[0]
-            if task in metrics_by_task:
-                metrics_by_task[task].append(value)
-            else:
-                metrics_by_task[task] = [value]
-        best_metrics_by_task = {}
-        exact_match_total = []
-        for task, values in metrics_by_task.items():
-            max_value = max(values)
-            best_metrics_by_task[task] = max_value
-            exact_match_total.append(max_value)
-        
-        exact_match = sum(exact_match_total) / len(exact_match_total)
-        best_metrics_by_task["exact_match"] = exact_match
+    metrics = json.load(open(metrics_path))
+    # group by tasks
+    metrics_by_task = {}
+    for key, value in metrics.items():
+        if not key.startswith("predict_exact_match_for_task"):
+            continue
+        task = key.split("_gpt")[0]
+        if task in metrics_by_task:
+            metrics_by_task[task].append(value)
+        else:
+            metrics_by_task[task] = [value]
+    best_metrics_by_task = {}
+    exact_match_total = []
+    for task, values in metrics_by_task.items():
+        max_value = max(values)
+        best_metrics_by_task[task] = max_value
+        exact_match_total.append(max_value)
+    
+    exact_match = sum(exact_match_total) / len(exact_match_total)
+    best_metrics_by_task["exact_match"] = exact_match
 
-        with open(output_path, 'w') as output_file:
-            json.dump(best_metrics_by_task, output_file, indent=4)
+    with open(output_path, 'w') as output_file:
+        json.dump(best_metrics_by_task, output_file, indent=4)
+
+def find_k_paraphrase_avg_metrics(metrics_path, output_path, k=1):
+    metrics = json.load(open(metrics_path))
+    # group by tasks
+    metrics_by_task = {}
+    for key, value in metrics.items():
+        if not key.startswith("predict_exact_match_for_task"):
+            continue
+        task = key.split("_gpt")[0]
+        if task in metrics_by_task:
+            metrics_by_task[task].append(value)
+        else:
+            metrics_by_task[task] = [value]
+    avg_metrics_by_task = {}
+    exact_match_total = []
+    for task, values in metrics_by_task.items():
+        sampled_values = random.sample(values, k) # sample without replacement
+        avg_value = np.mean(sampled_values)
+        avg_metrics_by_task[task] = avg_value
+        exact_match_total.append(avg_value)
+    
+    exact_match = sum(exact_match_total) / len(exact_match_total)
+    avg_metrics_by_task["exact_match"] = exact_match
+
+    with open(output_path, 'w') as output_file:
+        json.dump(avg_metrics_by_task, output_file, indent=4)
         
 
 if __name__=="__main__":
-        # find majority vote
-        # find_majority_vote_metrics(predictions_path, output_path)
+    # find majority vote
+    find_majority_vote_metrics(args.predictions_path, args.output_path)
 
-        # find best paraphrase
-        find_best_paraphrase_metrics(metrics_path, output_path)
+    # find best paraphrase
+    # find_best_paraphrase_metrics(args.metrics_path, args.output_path)
+
+    # find average of k randomly sampled paraphrase
+    # find_k_paraphrase_avg_metrics(args.metrics_path, args.output_path, k=args.k)
