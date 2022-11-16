f13d70f0a4297a0acf5892f3791bb45bcf8b3d3c
Shirley Zhang <shirleyzhang2@yahoo.com>
2022-11-16 20:52:16 +0000

    Add k=1,2,4,8 trials

--- Tk-Instruct/src/ensemble_paraphrased_prompts.py
+++ Tk-Instruct/src/ensemble_paraphrased_prompts.py
@@ -8,16 +8,13 @@ from compute_metrics import compute_grouped_metrics
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', "--predictions_path", default="../output/textual_entailment/tk-instruct-small-def-pos/predict_eval_predictions_all_paraphrases.jsonl", help="input path to load predictions jsonl for all paraphrased prompts")
 parser.add_argument('-m', "--metrics_path", default="../output/textual_entailment/tk-instruct-small-def-pos/predict_results_all_paraphrases.json", help="input path to load metrics json for all paraphrased prompts")
-parser.add_argument('-o', "--output_path", default="../output/textual_entailment/tk-instruct-small-def-pos/predict_results_ensemble_k=8.json", help="output path to save metrics, e.g. best, ensemble, k_random_avg")
-parser.add_argument('-k', "--k", default=4, help="number of paraphrased prompts to sample")
-parser.add_argument('-d', "--d", default=8, help="value of k after downsampling (e.g., d=8 means using k=8 instead of k=16")
+parser.add_argument('-o', "--output_path", default="../output/textual_entailment/tk-instruct-small-def-pos/predict_results_ensemble_avg_k=1.json", help="output path to save metrics, e.g. best, ensemble, k_random_avg")
+parser.add_argument('-k', "--k", default=1, help="number of paraphrased prompts to sample")
 args = parser.parse_args()
 
-def find_majority_vote_metrics(predictions_path, output_path, downsample):
+def find_majority_vote_metrics(predictions_path, output_path, k):
     with open(predictions_path, 'r') as json_file:
         json_list = list(json_file)
-    # downsample
-    json_list = random.sample(json_list, downsample)
 
     predictions_by_instanceid = {}
     for entry in json_list:
@@ -27,7 +24,6 @@ def find_majority_vote_metrics(predictions_path, output_path, downsample):
                 predictions_by_instanceid[instance_id] = [d]
         else:
                 predictions_by_instanceid[instance_id].append(d)
-
     predictions = []
     references = []
     groups = []
@@ -35,7 +31,9 @@ def find_majority_vote_metrics(predictions_path, output_path, downsample):
         reference = ds[0]["Instance"]["output"]
         task = ds[0]["Task"].split("_gpt")[0]
         preds = [d["Prediction"] for d in ds]
-        c = Counter(preds)
+        #downsample
+        sampled_preds = random.sample(preds, k)
+        c = Counter(sampled_preds)
         prediction, _ = c.most_common()[0]
         predictions.append(prediction)
         references.append(reference)
@@ -43,7 +41,6 @@ def find_majority_vote_metrics(predictions_path, output_path, downsample):
 
 
     metrics = compute_grouped_metrics(predictions, references, groups, xlingual=False)
-
     exact_match_total = []
     for key, value in metrics.items():
             if key.startswith("exact_match"):
@@ -54,10 +51,8 @@ def find_majority_vote_metrics(predictions_path, output_path, downsample):
     with open(output_path, 'w') as output_file:
         json.dump(metrics, output_file, indent=4)
 
-def find_best_paraphrase_metrics(metrics_path, output_path, downsample):
+def find_best_paraphrase_metrics(metrics_path, output_path, k):
     metrics = json.load(open(metrics_path))
-    #downsample
-    metrics = random.sample(metrics, downsample)
     # group by tasks
     metrics_by_task = {}
     for key, value in metrics.items():
@@ -71,7 +66,9 @@ def find_best_paraphrase_metrics(metrics_path, output_path, downsample):
     best_metrics_by_task = {}
     exact_match_total = []
     for task, values in metrics_by_task.items():
-        max_value = max(values)
+        # downsample
+        sampled_values = random.sample(values, k)
+        max_value = max(sampled_values)
         best_metrics_by_task[task] = max_value
         exact_match_total.append(max_value)
     
@@ -81,10 +78,8 @@ def find_best_paraphrase_metrics(metrics_path, output_path, downsample):
     with open(output_path, 'w') as output_file:
         json.dump(best_metrics_by_task, output_file, indent=4)
 
-def find_k_paraphrase_avg_metrics(metrics_path, output_path, downsample, k=1):
+def find_k_paraphrase_avg_metrics(metrics_path, output_path, k):
     metrics = json.load(open(metrics_path))
-    # downsample
-    metrics = random.sample(metrics, downsample)
     # group by tasks
     metrics_by_task = {}
     for key, value in metrics.items():
@@ -98,6 +93,7 @@ def find_k_paraphrase_avg_metrics(metrics_path, output_path, downsample, k=1):
     avg_metrics_by_task = {}
     exact_match_total = []
     for task, values in metrics_by_task.items():
+        # downsample
         sampled_values = random.sample(values, k) # sample without replacement
         avg_value = np.mean(sampled_values)
         avg_metrics_by_task[task] = avg_value
@@ -112,10 +108,10 @@ def find_k_paraphrase_avg_metrics(metrics_path, output_path, downsample, k=1):
 
 if __name__=="__main__":
     # find majority vote
-    find_majority_vote_metrics(args.predictions_path, args.output_path, args.downsample)
+    #find_majority_vote_metrics(args.predictions_path, args.output_path, args.k)
 
     # find best paraphrase
-    # find_best_paraphrase_metrics(args.metrics_path, args.output_path, args.downsample)
+    #find_best_paraphrase_metrics(args.metrics_path, args.output_path, args.k)
 
     # find average of k randomly sampled paraphrase
-    # find_k_paraphrase_avg_metrics(args.metrics_path, args.output_path, args.downsample, k=args.k)
+    find_k_paraphrase_avg_metrics(args.metrics_path, args.output_path, k=args.k)
