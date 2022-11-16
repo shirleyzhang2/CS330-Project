bb60a161dc4775be9463b932fdc3d1d78b41e27c
Violet Yao <violetyao@CS330HW2.ntri1yosa5je5oxfryslwk3cje.xx.internal.cloudapp.net>
2022-11-16 09:00:44 +0000

    add 16 paraphrased gpt3 tasks

--- generate_gpt3_tasks.py
+++ generate_gpt3_tasks.py
@@ -4,9 +4,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', "--input", default="tasks", help="input task file or dir containing task files")
-parser.add_argument('-p', "--prompt", default="gpt3-augment-results", help="input paraphrased prompt file or dir containing paraphrased prompt files")
-parser.add_argument('-l', "--list", default="eval/textual_entailment_gpt3_augment.txt", help="output file for list of paraphrased tasks")
-parser.add_argument('-o', "--output", default="gpt3-augment-tasks", help="output dir for tasks with paraphrased prompts")
+parser.add_argument('-p', "--prompt", default="gpt3-paraphrase-results", help="input paraphrased prompt file or dir containing paraphrased prompt files")
+parser.add_argument('-l', "--list", default="eval/textual_entailment_gpt3_paraphrase.txt", help="output file for list of paraphrased tasks")
+parser.add_argument('-o', "--output", default="gpt3-paraphrase-tasks", help="output dir for tasks with paraphrased prompts")
+parser.add_argument('-e', "--engine", default="text-davinci-002", help="GPT3 engine used for generating paraphrase/augmentation")
 args = parser.parse_args()
 
 if not os.path.exists(args.output):
@@ -23,6 +24,9 @@ for task in prompt_paths:
     paraphrased_prompt_dict = json.load(open(task, 'r'))
     task_name = paraphrased_prompt_dict['orignal_task']
     generated_prompts = paraphrased_prompt_dict['generated_prompts']
+    engine = paraphrased_prompt_dict['engine'] if 'engine' in paraphrased_prompt_dict else args.engine
+    if args.engine != engine:
+        continue
 
     task_dir = os.path.join(args.input, task_name)
     task_dict = json.load(open(task_dir, 'r', encoding="utf-8"))
